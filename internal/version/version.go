5fc005fda33930e84e6678e7648a5eb3b460e9d7
Rodrigo Brito <rodrigo@w3ti.com.br>
2026-07-16 07:54:24 -0300

    release: Vega 2.0.1

--- internal/version/version.go
+++ internal/version/version.go
@@ -2,4 +2,4 @@ package version
 
 // Version is overridden at build time via:
 //   go build -ldflags "-X github.com/lyraos/vegad/internal/version.Version=x.y.z"
-var Version = "2.0.0"
+var Version = "2.0.1"
